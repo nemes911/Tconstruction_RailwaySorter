ff838303b2b2f07a56c9d671e3a9be7f9267383e
dolgi@.com <sokolovpi-23B>
2025-10-31 22:32:42 +0300

    Добавлено Логирование в json

--- TConstruction/Form1.cs
+++ TConstruction/Form1.cs
@@ -9,12 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using TConstruction.Controllers;
 using TConstruction.Models;
+using TConstruction.Utils;
 
 namespace TConstruction
 {
     public partial class Form1 : Form
     {
         private readonly ControllSorted controller;
+
+        private static readonly Random rnd = new Random();
+
+        private SpeedTrain currentTrain;
 
         public Form1()
         {
@@ -31,15 +36,42 @@ namespace TConstruction
         {
             pictureBox1.Left -= 2;
             if (pictureBox1.Left <= panel6.Left)
+            {
                 timer1.Stop();
+
+                if (currentTrain != null)
+                {
+                    Logger.Log(new JsonModelLog(
+                    currentTrain.id,
+                    currentTrain.name,
+                    "Left",
+                    DateTime.Now
+
+                ));
+                }
+            }
         }
 
         private void MoveRight(object sender, EventArgs e)
         {
             pictureBox1.Left += 2;
             if (pictureBox1.Left >= panel1.Right)
+            {
                 timer1.Stop();
+                if (currentTrain != null)
+                {
+                    Logger.Log(new JsonModelLog(
+                    currentTrain.id,
+                    currentTrain.name,
+                    "Right",
+                    DateTime.Now
+
+                ));
+                }
+            }
         }
+
+
 
 
         private void timer1_Tick(object sender, EventArgs e)//timer
@@ -83,15 +115,44 @@ namespace TConstruction
         {
 
         }
-
-        private async void button1_Click(object sender, EventArgs e)// спавн поезда 
+        private SpeedTrain GenerateRandomTrain()
         {
-            var train = new SpeedTrain {id = 52, name = "train1", speed = 120 };
+            int id = rnd.Next(1000, 9999); // случайный ID от 1000 до 9999
 
-            await controller.HandleTrainAsync(train);
+            string[] names = { "Orion", "Falcon", "Comet", "Vega", "Nova", "Titan", "Echo", "Zephyr" };
+            string name = names[rnd.Next(names.Length)];
 
-            pictureBox1.Location = new Point(panel6.Left, panel2.Bottom - pictureBox1.Height);
+            return new SpeedTrain
+            {
+                id = id,
+                name = name,
+                speed = 120
+            };
+        }
+
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            currentTrain = GenerateRandomTrain();
+
+            await controller.HandleTrainAsync(currentTrain);
+
+            pictureBox1.Location = new Point(
+                panel2.Left - pictureBox1.Width / 2,
+                panel2.Bottom - pictureBox1.Height
+            );
+
+            Logger.Log(new JsonModelLog(currentTrain.id, currentTrain.name, "Spawned", DateTime.Now));
+
+            // Сброс подписок
+            timer1.Stop();
+            timer1.Tick -= MoveLeft;
+            timer1.Tick -= MoveRight;
+            timer1.Tick -= timer1_Tick;
+            timer1.Tick += timer1_Tick;
+
             timer1.Start();
         }
+
     }
 }
--- TConstruction/Utils/Logger.cs
+++ TConstruction/Utils/Logger.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TConstruction.Models;
+using System.IO;
+using System.Text.Json;
 
 namespace TConstruction.Utils
 {
     internal class Logger
     {
-        public static void Log(string message)
+        private static readonly string logPath = "train_log.json";
+
+        public static void Log(JsonModelLog jsonModelLog)
         {
-            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
-            // Дополнительно: запись в файл 
+            Console.WriteLine($"[{jsonModelLog.date:HH:mm:ss}] {jsonModelLog.name} → {jsonModelLog.operation}");
+
+            var json = System.Text.Json.JsonSerializer.Serialize(jsonModelLog);
+            File.AppendAllText(logPath, json + Environment.NewLine);
         }
     }
 }
